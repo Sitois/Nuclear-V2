6d1bcb5180d70de112f2c7cefe47d1444efbe80f
Sitois <143238636+Sitois@users.noreply.github.com>
2024-04-08 15:34:41 +0200

    second commit

---
+++ config_selfbot.py
@@ -0,0 +1,56 @@
+#######################
+#  selfbot            #
+#        basic        #
+#         config ^^   #
+#######################
+
+# en: SelfBot name
+# fr: Nom du SelfBot
+selfbot_name = "Nuclear Selfbot"
+
+# en: Account Token.
+# fr: Token du compte.
+token = ""
+
+# en: Commands prefix.
+# fr: Prefix des commandes.
+prefix = "&"
+
+# fr: Langue.
+# en: Language.
+lang = "en" # fr / en
+
+# fr: Default Nitro Sniper mode.
+# fr: Mode du Nitro Sniper par défaut. (True=On, False=Off)
+nitro_sniper = False
+
+# en: Commands delay of delete.
+# fr: Délai de supression des commandes.
+deltime = 20
+########################
+
+
+#######################
+# fr: RPC par défaut  #
+# en: Default RPC     #
+#######################
+
+activity_name = "☄"
+activity_details = " "
+activity_state = " "
+
+streaming_url = "https://twitch.tv/twitch"
+activity_button_one = "Nuclear !"
+activity_button_one_answer = "https://github.com/Sitois/Nuclear"
+activity_button_two = "Star it!"
+activity_button_two_answer = "https://github.com/Sitois/Nuclear"
+
+# see &tuto
+assets = {"large_image": "mp:attachments/1135264530188992562/1205872002238382111/PNjYcIL.png?ex=661a8c51&is=66081751&hm=791e0e36c6fe078da6c10af86c4c541f5eb0ca78157cdc4b84f4e0d70af88f15&=&format=webp&quality=lossless",
+          "large_text": "☄",
+          "small_image": "mp:attachments/1135264530188992562/1198617586389225592/aBjaPbQ.png?ex=661bd79d&is=6609629d&hm=2aff7ae01882753b086aac14119f874afbbfceacde64c17e6701e26350f67840&=&format=webp&quality=lossless",
+          "small_text": "☄"
+          }
+
+
+#################
