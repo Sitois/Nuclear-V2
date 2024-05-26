9f2a3a9e70e3c840c72a1a2c57f420e3b518932d
Sitois <143238636+Sitois@users.noreply.github.com>
2024-05-26 13:28:12 +0200

    Add: Version warning!

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ except ImportError:
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
-nuclear_version = "v1.7"
+nuclear_version = "v1.8"
 
 print(fr"""{Fore.LIGHTCYAN_EX}$$\   $$\                     $$\                               
 $$$\  $$ |                    $$ |                              
@@ -91,6 +91,17 @@ def check_latest_version(repo_owner, repo_name):
         return latest_version
     else:
         return None
+
+# Prevent from starting the selfbot with discord.py==1.7.3
+if discord.__version__ == "1.7.3":
+    print(f"{Fore.RED}[CRITICAL] {langs.error_discord_version[config_selfbot.lang]} https://github.com/Sitois/Nuclear/releases/tag/{check_latest_version('Sitois', 'Nuclear-V2')}{Style.RESET_ALL}")
+    exit()
+
+# Prevent from starting the selfbot with the broken pip version
+if discord.__version__ == "2.0.0":
+    print(f"{Fore.RED}[CRITICAL] {langs.error_discord_version[config_selfbot.lang]} https://github.com/Sitois/Nuclear/releases/tag/{check_latest_version('Sitois', 'Nuclear-V2')}{Style.RESET_ALL}")
+    exit()
+
 
 def call_check_repo():
     global nuclear_version
@@ -215,9 +226,12 @@ async def on_ready():
                               afk=True,
                               idle_since=datetime.datetime(today_date.year, today_date.month, today_date.day))
 
+
     # TODO:
-    # Add: Nuclear Panel: Create a group, rename it to "Nuclear Panel" and end a welome message,
+    # Add: Nuclear Panel: Create a group, rename it to "Nuclear Panel" and a welcome message,
     # i.g.: "Don't use command in servers you can get reported, here the support link, offcial github link, current version" etc..
+
+    # TODO: Servers backup.
     """
     if not rpc.read_variable_json("first_start"):
         random_user = random.choice(bot.friends)
@@ -245,7 +259,6 @@ async def stop(ctx):
     await ctx.message.delete()
     await bot.close()
     exit()
-
 
 #############
 #############
