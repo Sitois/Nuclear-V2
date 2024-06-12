12f3a608094e7d44b72038caffbfac72ac104658
Sitois <143238636+Sitois@users.noreply.github.com>
2024-06-12 15:01:16 +0200

    Renamed `utils.py` to `util.py` to avoid confusions with `utils` floder

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,5 +1,5 @@
 from .help import HelpCommands
-from .utils import UtilsCommands
+from .util import UtilsCommands
 from .fun import FunCommands
 from .voice import VoiceCommands
 from .config import ConfigCommands
