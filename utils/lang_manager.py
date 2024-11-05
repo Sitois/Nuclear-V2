6bad273bd8ae432d62abd977ad57d33bf5f3cd31
Andor <146471594+Andor-py@users.noreply.github.com>
2024-11-05 05:56:25 +0100

    commented lang_manager.py

--- utils/lang_manager.py
+++ utils/lang_manager.py
@@ -1,169 +1,169 @@
 """
-The MIT License (MIT)
+La licence MIT (MIT)
 
-Copyright (c) 2024-present Lenochxd
+Copyright (c) 2024-présent Lenochxd
 
-Permission is hereby granted, free of charge, to any person obtaining a
-copy of this software and associated documentation files (the "Software"),
-to deal in the Software without restriction, including without limitation
-the rights to use, copy, modify, merge, publish, distribute, sublicense,
-and/or sell copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following conditions:
+La permission est par la présente accordée, sans frais, à toute personne obtenant une
+copie de ce logiciel et des fichiers de documentation associés (le "Logiciel"),
+de traiter le Logiciel sans restriction, y compris sans limitation
+les droits d'utiliser, copier, modifier, fusionner, publier, distribuer, sous-licencier,
+et/ou vendre des copies du Logiciel, et de permettre aux personnes à qui le
+Logiciel est fourni de le faire, sous réserve des conditions suivantes :
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+Le présent avis de copyright et cet avis de permission doivent être inclus dans
+toutes les copies ou portions substantielles du Logiciel.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-DEALINGS IN THE SOFTWARE.
+LE LOGICIEL EST FOURNI "EN L'ÉTAT", SANS GARANTIE D'AUCUNE SORTE, EXPRESSE
+OU IMPLICITE, Y COMPRIS MAIS SANS LIMITATION LES GARANTIES DE COMMERCIALISATION,
+D'ADÉQUATION À UN OBJECTIF PARTICULIER ET DE NON-VIOLATION. EN AUCUN CAS, LES
+AUTEURS OU DÉTENTEURS DE DROITS D'AUTEUR NE POURRONT ÊTRE TENUS RESPONSABLES DE TOUTE RÉCLAMATION, DOMMAGE OU AUTRE
+RESPONSABILITÉ, QUE CE SOIT DANS LE CADRE D'UN CONTRAT, DÉLIT OU AUTRE, DÉCOULANT
+DE OU EN LIAISON AVEC LE LOGICIEL OU L'UTILISATION OU AUTRES
+TRAITEMENTS DANS LE LOGICIEL.
 """
 
-# NOTE: Language system made by Lenochxd, check her github(https://github.com/Lenochxd) !
+# REMARQUE : Système de langue créé par Lenochxd, consultez son GitHub (https://github.com/Lenochxd) !
 
 import os
 
 import config_selfbot
 
 class Lang():
-    """Constructor.
+    """Constructeur.
 
-    Parameters
+    Paramètres
     ----------
     path: :class:`str`
-        The path of the folder containing .lang translations.
-        'r string' are recommended (i.g.: r'.\langs').
+        Le chemin du dossier contenant les traductions .lang.
+        Les chaînes 'r string' sont recommandées (ex.: r'.\langs').
 
-        Default to r"./translations".
+        Défaut à r"./translations".
 
     default_language: :class:`str`
-        The default language to use (if the text wasn't found on the specified language).
+        La langue par défaut à utiliser (si le texte n'a pas été trouvé dans la langue spécifiée).
 
-        Default to "en_US".
+        Défaut à "en_US".
 
-    Raises
+    Lève
     ------
     KeyError
-        No path were given or given path doesn't exists.
+        Aucun chemin n'a été donné ou le chemin donné n'existe pas.
     """
     def __init__(self,
                  path: str = r".\translations",
                  default_language: str = "en_US"):
-        self.path: str = path
+        self.path: str = path  # Chemin du dossier de traductions
         if not os.path.exists(self.path):
-            raise KeyError("No path were given or given path doesn't exists.")
-        self.lang_files: dict = {}
-        self.default_language: str = default_language
-        # Load all lang files at class init.
+            raise KeyError("Aucun chemin n'a été donné ou le chemin donné n'existe pas.")
+        self.lang_files: dict = {}  # Dictionnaire pour stocker les fichiers de langue
+        self.default_language: str = default_language  # Langue par défaut
+        # Charger tous les fichiers de langue à l'initialisation de la classe.
         self.load_all_lang_files()
 
     def load_lang_file(self, lang: str) -> dict:
-        """Loads .lang file.
+        """Charge le fichier .lang.
 
-        Parameters
+        Paramètres
         ----------
         lang: :class:`str`
-            The lang name (i.g.: en_US, fr_FR...).
+            Le nom de la langue (ex.: en_US, fr_FR...).
 
-        Raises
+        Lève
         ------
         TypeError
-            Lang file error.
+            Erreur de fichier de langue.
 
-        Returns
+        Retourne
         -------
         lang_dictionary: :class:`dict`
-            A dict version of the the loaded .lang file.
+            Une version dict du fichier .lang chargé.
         """
 
         lang_dictionary = {}
-        lang_path = f"{self.path}/{lang}.lang"
-        if not os.path.isfile(f"{self.path}/{lang}.lang"):
+        lang_path = f"{self.path}/{lang}.lang"  # Chemin du fichier de langue
+        if not os.path.isfile(f"{self.path}/{lang}.lang"):  # Vérifie si le fichier existe
             for root, dirs, files in os.walk(self.path):
                 for file in files:
-                    if file.endswith(".lang") and file.startswith(lang):
+                    if file.endswith(".lang") and file.startswith(lang):  # Recherche le fichier de langue
                         lang_path = f"./{self.path}/{file}"
 
-        with open(lang_path, "r", encoding="utf-8") as f:
+        with open(lang_path, "r", encoding="utf-8") as f:  # Ouvre le fichier de langue
             lines = f.readlines()
             for line in lines:
-                if (line.replace(" ", "").replace("\n", "") != ""
-                    and not line.startswith("//")
+                if (line.replace(" ", "").replace("\n", "") != ""  # Ignore les lignes vides
+                    and not line.startswith("//")  # Ignore les commentaires
                     and not line.startswith("#")):
                     try:
-                        key, value = line.strip().replace("\n", "").split("=", 1)
-                        lang_dictionary[key] = value.strip()
+                        key, value = line.strip().replace("\n", "").split("=", 1)  # Sépare la clé et la valeur
+                        lang_dictionary[key] = value.strip()  # Ajoute au dictionnaire
                     except Exception as e:
                         line = line.replace("\n", "")
-                        raise TypeError(f'\nLANG FILE ERROR:\nLine: {line}\nError: {e}\n')
+                        raise TypeError(f'\nERREUR DE FICHIER LANG:\nLigne: {line}\nErreur: {e}\n')
         return lang_dictionary
 
     def load_all_lang_files(self, path: str = None) -> dict:
-        """Loads all .lang files.
+        """Charge tous les fichiers .lang.
 
-        Parameters
+        Paramètres
         ----------
         path: :class:`str`
-            The path of the folder containing .lang translations.
+            Le chemin du dossier contenant les traductions .lang.
 
-            Default to the contructor path.
+            Défaut au chemin du constructeur.
 
-        Returns
+        Retourne
         -------
         lang_files: :class:`dict`
-            A dict version of the the loaded .lang files.
+            Une version dict des fichiers .lang chargés.
         """
 
         if path is None:
             path = self.path
 
-        for root, dirs, files in os.walk(self.path):
+        for root, dirs, files in os.walk(self.path):  # Parcourt tous les fichiers dans le dossier
             for file in files:
-                if file.endswith(".lang"):
-                    lang = file.split(".")[0]
-                    self.lang_files[lang] = self.load_lang_file(lang)
+                if file.endswith(".lang"):  # Vérifie si c'est un fichier de langue
+                    lang = file.split(".")[0]  # Extrait le nom de la langue
+                    self.lang_files[lang] = self.load_lang_file(lang)  # Charge le fichier de langue
         return self.lang_files
 
     def reload_all_lang_files(self) -> None:
-        """Reloads all .lang files."""
+        """Recharge tous les fichiers .lang."""
         self.lang_files = self.load_all_lang_files(self.path)
 
     def language_exists(self, lang: str = None) -> bool:
-        """Check whatever a language exists in the translations folder.
+        """Vérifie si une langue existe dans le dossier de traductions.
 
-        Parameters
+        Paramètres
         ----------
         lang: :class:`str`
-            The language to check.
+            La langue à vérifier.
 
-        Returns
+        Retourne
         --------
         :class:`bool`
-            Whatever if the given language exists.
+            Vérifie si la langue donnée existe.
         """
 
         lang = next((l for l in self.lang_files if l.lower().startswith(lang.lower())), '')
         return lang in self.lang_files
 
     def languages(self) -> list[dict]:
-        """Retrieve available languages.
+        """Récupère les langues disponibles.
 
-        Returns
+        Retourne
         -------
         languages: :class:`list[dict]`
-            A :class:`list` containing :class:`dict` with lang informations.
+            Une :class:`list` contenant :class:`dict` avec les informations sur les langues.
         """
 
         languages_info = []
-        for lang in self.lang_files:
+        for lang in self.lang_files:  # Parcourt les fichiers de langue chargés
             languages_info.append({
-                'name': lang,
-                'code': self.lang_files[lang]['lang_code'],
-                'native_name': self.lang_files[lang]['lang_name'],
-                'credits': self.lang_files[lang]['credits'],
+                'name': lang,  # Nom de la langue
+                'code': self.lang_files[lang]['lang_code'],  # Code de la langue
+                'native_name': self.lang_files[lang]['lang_name'],  # Nom natif de la langue
+                'credits': self.lang_files[lang]['credits'],  # Crédits pour la langue
             })
 
         return languages_info
@@ -171,48 +171,48 @@ class Lang():
     def text(self,
              text: str = None,
              lang: str = None) -> str:
-        """Returns the given text in the given language.
+        """Retourne le texte donné dans la langue spécifiée.
 
-        Parameters
+        Paramètres
         ----------
         text: :class:`str`
-            The text to get in the given language.
+            Le texte à obtenir dans la langue donnée.
         lang: :class:`str`
-            The language to get the text into.
+            La langue dans laquelle obtenir le texte.
 
-            Default to the default language.
+            Défaut à la langue par défaut.
 
-        Raises
+        Lève
         ------
         KeyError
-            Given text wasn't found on both default language and given language.
+            Le texte donné n'a pas été trouvé dans la langue par défaut et dans la langue donnée.
 
-        Returns
+        Retourne
         -------
         :class:`str`
-            The translated text.
+            Le texte traduit.
         """
 
         if text is None:
             return ""
 
         if lang is None:
-            lang = config_selfbot.lang
+            lang = config_selfbot.lang  # Récupère la langue par défaut de la config
 
         lang = next((l for l in self.lang_files if l.lower().startswith(lang.lower())), self.default_language)
 
         if not lang in self.lang_files:
-            lang = self.default_language
+            lang = self.default_language  # Utilise la langue par défaut si la langue spécifiée n'est pas trouvée
 
-        # TODO: Should improve here.
-        if not text in self.lang_files[lang]:
+        # TODO : Améliorer ici.
+        if not text in self.lang_files[lang]:  # Vérifie si le texte existe dans le fichier de langue
             try:
-                return self.lang_files[lang][text].replace(
+                return self.lang_files[lang][text].replace(  # Remplace les séquences d'échappement
             '\\n', '\n').replace(
             '\\r', '\r').replace(
-                '%prefix%', config_selfbot.prefix)
+                '%prefix%', config_selfbot.prefix)  # Remplace le préfixe par celui de la config
             except KeyError:
-                raise KeyError("Given text wasn't found on both default language and given language.")
+                raise KeyError("Le texte donné n'a pas été trouvé ni dans la langue par défaut ni dans la langue donnée.")
 
         return self.lang_files[lang][text].replace(
             '\\n', '\n').replace(
@@ -222,35 +222,16 @@ class Lang():
     def t(self,
           text: str = None,
           lang: str = None) -> str:
-        """Returns the given text in the given language.
+        """Retourne le texte donné dans la langue spécifiée.
 
-        Shortcut for Lang().text()
+        Raccourci pour Lang().text()
 
-        Parameters
+        Paramètres
         ----------
         text: :class:`str`
-            The text to get in the given language.
+            Le texte à obtenir dans la langue donnée.
         lang: :class:`str`
-            The language to get the text into.
+            La langue dans laquelle obtenir le texte.
 
-            Default to the default language.
+            Défaut à la langue par défaut.
 
-        Raises
-        ------
-        KeyError
-            Given text wasn't found on both default language and given language.
-
-        Returns
-        -------
-        :class:`str`
-            The translated text.
-        """
-
-        return self.text(text, lang)
-
-try:
-    lang = Lang(default_language="en_US",
-           path=r".\translations")
-except Exception:
-    lang = Lang(default_language="en_US",
-           path=r"..\translations")
