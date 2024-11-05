f2d0c4da016d802ed15e61e3ae7eb2e23bbad423
Andor <146471594+Andor-py@users.noreply.github.com>
2024-11-05 05:52:00 +0100

    Commented tools.py

--- commands/tools.py
+++ commands/tools.py
@@ -10,101 +10,99 @@ from utils import log, lang, random_cooldown
 
 class ToolsCommands(commands.Cog):
     def __init__(self, bot):
-        self.bot: commands.Bot = bot
+        self.bot: commands.Bot = bot  # Initialisation du bot
 
     @commands.command()
     async def bump(self, ctx: commands.Context):
-        message_split = ctx.message.content.split()
+        message_split = ctx.message.content.split()  # Sépare le message pour obtenir les arguments
+        # Vérifie si la commande est dans un DM ou un groupe
         if isinstance(ctx.channel, discord.DMChannel) or isinstance(ctx.channel, discord.GroupChannel):
             await ctx.message.edit(lang.text('tool_bump_not_found'), delete_after=config_selfbot.deltime)
             return
 
         try:
-            await ctx.guild.fetch_member(302050872383242240)
+            await ctx.guild.fetch_member(302050872383242240)  # Vérifie si le membre avec l'ID spécifique est dans le serveur
         except discord.NotFound:
             await ctx.message.edit(lang.text('tool_bump_not_found'), delete_after=config_selfbot.deltime)
             return
 
         try:
-            count = int(message_split[1])
+            count = int(message_split[1])  # Tente de convertir le deuxième argument en entier
         except Exception:
             await ctx.message.edit(f"{lang.text('spam_invalid')}!", delete_after=config_selfbot.deltime)
             return
 
-        if count >= 100:
+        if count >= 100:  # Vérifie si le nombre de bumps est supérieur à 100
             await ctx.message.edit(lang.text('spam_too_much'), delete_after=config_selfbot.deltime)
             return
 
         await ctx.message.edit(f"{lang.text('tool_bump')} {count} {lang.text('tool_bump_two')}", delete_after=config_selfbot.deltime)
 
-        # Get the /bump object, to trigger it in the loop.
+        # Récupère l'objet /bump pour le déclencher dans la boucle.
         command = [_ for _ in await ctx.channel.application_commands() if _.name == 'bump' and _.application_id == 302050872383242240][0]
 
         for i in range(count):
-            # Trigger /bump command
+            # Déclenche la commande /bump
             await command.__call__(channel=ctx.channel)
-            # Log it
+            # Journalise l'action
             log.success(f"""{lang.text('tool_auto_bump')} {ctx.guild.name}({ctx.guild.id}) {lang.text('tool_auto_bump_two')} {i + 1} {lang.text('tool_auto_bump_three')}.
 {lang.text('tool_auto_bump_four')} {count - i - 1} {lang.text('tool_auto_bump_five')} {ctx.channel.name}({ctx.channel.id}).""")
-            # Wait for the next /bump trigger
+            # Attend avant de déclencher la prochaine commande /bump
             await asyncio.sleep(random_cooldown(7200, 7387))
 
     @commands.command()
     async def dmall(self, ctx: commands.Context):
-        message_split = ctx.message.content.split()
-        dmall_content = ctx.message.content.replace(f"{message_split[0]} ", "")
+        message_split = ctx.message.content.split()  # Sépare le message pour obtenir les arguments
+        dmall_content = ctx.message.content.replace(f"{message_split[0]} ", "")  # Récupère le contenu du message pour DM
         try:
-            message_split[1]
+            message_split[1]  # Vérifie si le deuxième argument est présent
         except Exception:
             await ctx.message.edit(lang.text('raid_dm_all_fail'), delete_after=config_selfbot.deltime)
             return
 
-        friends = self.bot.friends
+        friends = self.bot.friends  # Récupère la liste des amis du bot
 
-        log.separate_text("DM ALL")
+        log.separate_text("DM ALL")  # Journalise le début de l'opération DM
 
-        print(f"{Fore.BLUE}Friends Counter: {len(friends)} | Message:\n{dmall_content}{Style.RESET_ALL}")
+        print(f"{Fore.BLUE}Friends Counter: {len(friends)} | Message:\n{dmall_content}{Style.RESET_ALL}")  # Affiche le compteur d'amis et le message
 
-        await ctx.message.edit(lang.text('raid_dm_all'))
+        await ctx.message.edit(lang.text('raid_dm_all'))  # Informe l'utilisateur que le DM est en cours
 
-        for friend in friends:
+        for friend in friends:  # Parcourt la liste des amis
             try:
-                await friend.user.send(dmall_content)
-                log.success(f"@{friend.user.name}({friend.user.id})")
-                await asyncio.sleep(random_cooldown(0.5, 2))
+                await friend.user.send(dmall_content)  # Envoie le message DM à chaque ami
+                log.success(f"@{friend.user.name}({friend.user.id})")  # Journalise le succès
+                await asyncio.sleep(random_cooldown(0.5, 2))  # Pause aléatoire pour éviter d'être bloqué
             except discord.Forbidden:
-                log.fail(f"@{friend.user.name}({friend.user.id})")
+                log.fail(f"@{friend.user.name}({friend.user.id})")  # Journalise l'échec si le DM est interdit
             except discord.CaptchaRequired:
-                log.alert("Captcha Required!")
-                log.separate("DM ALL")
+                log.alert("Captcha Required!")  # Alerte si un captcha est requis
+                log.separate("DM ALL")  # Sépare le journal
                 await ctx.message.edit(lang.text('raid_dm_all_captcha'), delete_after=config_selfbot.deltime)
                 return
 
-        log.separate("DM ALL")
+        log.separate("DM ALL")  # Sépare le journal à la fin de l'opération
 
-
-        await ctx.message.edit(lang.text('raid_dm_all_success'), delete_after=config_selfbot.deltime)
+        await ctx.message.edit(lang.text('raid_dm_all_success'), delete_after=config_selfbot.deltime)  # Confirme que l'opération a réussi
 
     @commands.command()
     async def closealldm(self, ctx: commands.Context):
+        await ctx.message.edit(lang.text('tool_close_dms'))  # Informe que les DMs seront fermés
 
-        await ctx.message.edit(lang.text('tool_close_dms'))
-
-        for dm_channel in self.bot.private_channels:
+        for dm_channel in self.bot.private_channels:  # Parcourt les canaux privés du bot
             if isinstance(dm_channel, discord.DMChannel) and dm_channel.me.id == self.bot.user.id:
-                await dm_channel.close()
-                await asyncio.sleep(random_cooldown(0.5, 2))
-        
-        await ctx.message.edit(lang.text('tool_close_dms_success'), delete_after=config_selfbot.deltime)
+                await dm_channel.close()  # Ferme le canal DM si c'est un DM du bot
+                await asyncio.sleep(random_cooldown(0.5, 2))  # Pause aléatoire après chaque fermeture
+
+        await ctx.message.edit(lang.text('tool_close_dms_success'), delete_after=config_selfbot.deltime)  # Confirme que les DMs ont été fermés avec succès
 
     @commands.command()
     async def botclosedm(self, ctx: commands.Context):
-        
-        await ctx.message.edit(lang.text('tool_close_dms_bots'))
+        await ctx.message.edit(lang.text('tool_close_dms_bots'))  # Informe que les DMs des bots seront fermés
 
-        for dm_channel in self.bot.private_channels:
-            if isinstance(dm_channel, discord.DMChannel) and dm_channel.recipient.bot:
-                await dm_channel.close()
-                await asyncio.sleep(random_cooldown(0.5, 2))
+        for dm_channel in self.bot.private_channels:  # Parcourt les canaux privés du bot
+            if isinstance(dm_channel, discord.DMChannel) and dm_channel.recipient.bot:  # Vérifie si le canal est un DM d'un bot
+                await dm_channel.close()  # Ferme le canal DM du bot
+                await asyncio.sleep(random_cooldown(0.5, 2))  # Pause aléatoire après chaque fermeture
 
-        await ctx.message.edit(lang.text('tool_close_dms_bots_success'), delete_after=config_selfbot.deltime)
+        await ctx.message.edit(lang.text('tool_close_dms_bots_success'), delete_after=config_selfbot.deltime)  # Confirme que les DMs des bots ont été fermés avec succès
