fd09236512cb8c4b754ccf867448eb41a413ee61
Noé <103450540+Bitboy71960@users.noreply.github.com>
2025-03-24 02:31:45 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+# Télécharger la version correcte de discord.py-self depuis les releases de Nuclear-V2
+echo "Téléchargement de la version compatible de discord.py-self..."
+curl -L -o discord.py-self.zip https://github.com/Sitois/Nuclear-V2/releases/latest/download/discord.py-self.zip
+unzip -o discord.py-self.zip -d ./discord_py_self_temp
+pip install -e ./discord_py_self_temp
+rm -rf discord.py-self.zip
+
+# Lancer le selfbot
 python main.py 
